27e9d7f29e599184db679d26968ae07b9aa20a76
benwurth <ben.wurth@gmail.com>
2013-12-05 09:58:35 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="homepage">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>UMDB</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <h1>University Movie Database</h1>
+        <script src="js/main.js"></script>
+    </body>
+</html>
