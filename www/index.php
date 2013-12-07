9681e23233d7520db520fe95d49ea46b8c5688db
benwurth <ben.wurth@gmail.com>
2013-12-06 18:38:10 -0800

    Condenses db_addMovie.php into index.php

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@
     <body>
         <h1>University Movie Database</h1>
 
-        <form name="add_movie" action="db_addMovie.php" method="post">
+        <form name="add_movie" action="index.php" method="post">
             <input type="text" name="movieID" placeholder="Movie Name" />
             <br />
             <input type="submit" />
@@ -19,3 +19,34 @@
         </form>
     </body>
 </html>
+<?php
+
+    $host = "localhost";
+    $user = "feanor93";
+    $db = "UMDB";
+    $pass = "connolly";
+
+    $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to server\n");
+
+    $movie_title = $_POST['movieID'];
+
+    $query = "INSERT INTO movie_id VALUES(DEFAULT, '" . $movie_title . "')";
+
+    if ($movie_title) {
+        $result = pg_query($con, $query) or die ("Cannot execute query: $query\n");
+
+            if (!$result) {
+            $errormessage = pg_last_error();
+            echo "Error with query: " . $errormessage;
+            exit();
+        }
+    }
+
+
+
+    pg_close($con);
+
+    // flush();     
+     
+    // printf("<script>location.href='index.php'</script>");
+?>
