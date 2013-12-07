39ba27cb63c752d36a7250c0db6e4799182953ca
benwurth <ben.wurth@gmail.com>
2013-12-06 19:11:58 -0800

    Homepage outputs movie information in a table

--- www/index.php
+++ www/index.php
@@ -15,8 +15,49 @@
             <input type="text" name="movieID" placeholder="Movie Name" />
             <br />
             <input type="submit" />
-            <br />
         </form>
+        <br />
+        <?php
+
+            $host = "localhost";
+            $user = "feanor93";
+            $db = "UMDB";
+            $pass = "connolly";
+
+            $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to server\n");
+
+            $query = "SELECT * FROM movie_id";
+
+            $result = pg_query($con, $query) or die ("Cannot execute query: $query\n");
+
+            //echo $result;
+
+            $numrows = pg_num_rows($result);
+            
+        ?>
+                
+        <table border="0">
+        <tr>
+            <th>ID</th>
+            <th>Movie Title</th>
+        </tr>
+
+        <?
+            
+            for ($i=0; $i < $numrows; $i++) { 
+                $row = pg_fetch_array($result, $i);
+                echo "<tr>";
+                echo "<td>", $row["movie_id"], "</td>";
+                echo "<td>", $row["movie_title"], "</td>";
+                echo "</td>";
+            }
+
+            echo "</ul>";
+
+            pg_close($con);
+        ?>
+
+        </table>
     </body>
 </html>
 <?php
@@ -42,11 +83,5 @@
         }
     }
 
-
-
     pg_close($con);
-
-    // flush();     
-     
-    // printf("<script>location.href='index.php'</script>");
 ?>
