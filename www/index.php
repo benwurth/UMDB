12012c3f5f36115a9ab5149401040021d3207042
benwurth <ben.wurth@gmail.com>
2013-12-08 02:13:50 -0800

    Adds description to movies and homepage

--- www/index.php
+++ www/index.php
@@ -12,7 +12,9 @@
         <h1>University Movie Database</h1>
 
         <form name="add_movie" action="index.php" method="post">
-            <input type="text" name="movieID" placeholder="Movie Name" />
+            <input type="text" name="movieID" placeholder="Movie Name"/>
+            <br />
+            <textarea rows="4" cols="25" name="movieDescription" placeholder="Description"></textarea>
             <br />
             <input type="submit" />
         </form>
@@ -26,7 +28,7 @@
 
             $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to server\n");
 
-            $query = "SELECT * FROM movie_id";
+            $query = "SELECT * FROM movie_id LEFT OUTER JOIN description ON (movie_id.movie_id = description.description_movie_id) ORDER BY movie_id ASC;";
 
             $result = pg_query($con, $query) or die ("Cannot execute query: $query\n");
 
@@ -40,6 +42,8 @@
         <tr>
             <th>ID</th>
             <th>Movie Title</th>
+            <th>Description ID</th>
+            <th>Description</th>
         </tr>
 
         <?
@@ -49,6 +53,8 @@
                 echo "<tr>";
                 echo "<td>", $row["movie_id"], "</td>";
                 echo "<td>", $row["movie_title"], "</td>";
+                echo "<td>", $row["description_id"], "</td>";
+                echo "<td>", $row["description"], "</td>";
                 echo "</td>";
             }
 
@@ -62,6 +68,8 @@
 </html>
 <?php
 
+    $valid = false;
+
     $host = "localhost";
     $user = "feanor93";
     $db = "UMDB";
@@ -70,10 +78,14 @@
     $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to server\n");
 
     $movie_title = pg_escape_string( $_POST['movieID'] );
+    $movie_description = pg_escape_string( $_POST['movieDescription'] );
 
-    $query = "INSERT INTO movie_id VALUES(DEFAULT, '" . $movie_title . "')";
+    if ($movie_title and $movie_description) {
+        $valid = true;
+    }
 
-    if ($movie_title) {
+    if ($valid) {
+        $query = "INSERT INTO movie_id VALUES(DEFAULT, '" . $movie_title . "');";
         $result = pg_query($con, $query) or die ("Cannot execute query: $query\n");
 
         if (!$result) {
@@ -81,11 +93,19 @@
             echo "Error with query: " . $errormessage;
             exit();
         }
+
+        $query = "INSERT INTO description VALUES( DEFAULT, '" . $movie_description . "', currval('movie_id_movie_id_seq'::regclass) );";
+        $result = pg_query($con, $query) or die ("Cannot execute query: $query\n");
+        // $result = pg_query($con, $query) or die (pg_last_error());
+
+        if (!$result) {
+            $errormessage = pg_last_error();
+            echo "Error with query: " . $errormessage;
+            exit();
+        }
+
         pg_close($con);
         echo '<meta http-equiv="refresh" content="0">';
     }
-
-    
-
 
 ?>
